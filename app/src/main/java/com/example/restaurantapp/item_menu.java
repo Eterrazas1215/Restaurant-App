0d0151850e3ab40d862967349a9f29dd65724f13
eterrazas1215 <terrazas.eduardo95@gmail.com>
2022-05-10 00:38:55 -0700

    Item_menu added

---
+++ app/src/main/java/com/example/restaurantapp/item_menu.java
@@ -0,0 +1,49 @@
+package com.example.restaurantapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class item_menu extends AppCompatActivity {
+    public Button appetizer, food,dessert,drinks;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.item_menu);
+        appetizer = findViewById(R.id.appetizer_bttn);
+        food = findViewById(R.id.food_bttn);
+        dessert = findViewById(R.id.dessert_bttn);
+        drinks = findViewById(R.id.drinks_bttn);
+
+        food.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(item_menu.this, foodmenu.class);
+                startActivity(intent);
+            }
+        });
+        drinks.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent3 = new Intent(item_menu.this, foodmenu.class);
+                startActivity(intent3);
+            }
+        });
+        dessert.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent4 = new Intent(item_menu.this, foodmenu.class);
+                startActivity(intent4);
+            }
+        });
+        appetizer.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent2= new Intent(item_menu.this, foodmenu.class);
+                startActivity(intent2);
+            }
+        });
+    }
+}
