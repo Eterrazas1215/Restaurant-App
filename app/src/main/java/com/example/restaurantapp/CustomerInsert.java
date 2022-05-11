98dddf4cfb2101920053eebe4e66552dd1a37195
Brian <bramirez246@gmail.com>
2022-05-10 21:08:44 -0700

    added small changes to activity flow on customer side

--- app/src/main/java/com/example/restaurantapp/CustomerInsert.java
+++ app/src/main/java/com/example/restaurantapp/CustomerInsert.java
@@ -93,7 +93,7 @@ public class CustomerInsert extends AppCompatActivity {
         btnMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(CustomerInsert.this, item_menu.class);
+                Intent intent = new Intent(CustomerInsert.this, CustomerImageRecycler.class);
                 startActivity(intent);
             }
         });
